4e933d30d01f47327550fa6c4d3ecb06a57117fe
Antonio Pagano <apagano@wawand.co>
2014-08-05 10:24:23 -0500

    [cleaning] moving endpoint-adding methods to the Grape::Resources method

--- lib/grape/resources.rb
+++ lib/grape/resources.rb
@@ -11,45 +11,46 @@ module Grape
 
         raise Error("To use grape_resources on a given class it should inherit from ActiveRecord::Base.( at least for now buddy ;) )") unless clazz < ActiveRecord::Base
 
-        list_endpoint_for clazz
-        get_endpoint_for clazz
+        Grape::Resources.list_endpoint_for( clazz, self )
+        Grape::Resources.get_endpoint_for( clazz, self )
         
 
         route('POST', ["/#{singular_name}"], {})
         route('PUT', ["/#{singular_name}/:id"], {})
         route('DELETE', ["/#{singular_name}/:id"], {})
       end
+    end
+  end
 
-      def list_endpoint_for(clazz)
+  module Resources
+    class << self
+      
+      def list_endpoint_for(clazz, api_instance)
         plural_name   = clazz.name.pluralize.underscore
 
-        route('GET', ["/#{plural_name}"], {} ) do                   
+        api_instance.route('GET', ["/#{plural_name}"], {} ) do                   
           result = Grape::Resources.list(clazz, params)
           result
         end
       end
 
-      def get_endpoint_for(clazz)
+      def get_endpoint_for(clazz, api_instance)
         singular_name = clazz.name.underscore
 
-        route('GET', ["/#{singular_name}/:id"], {}) do
+        api_instance.route('GET', ["/#{singular_name}/:id"], {}) do
           result = Grape::Resources.find(clazz, params)
           error!( "#{singular_name} not found", 404) if result.nil?
           result
         end
       end
 
-    end
-  end
+      def list(clazz, params)
+        result = clazz.all
+      end
 
-  module Resources
-
-    def self.list(clazz, params)
-      result = clazz.all
-    end
-
-    def self.find(clazz, params)
-      result = clazz.find_by_id( params[:id])
+      def find(clazz, params)
+        result = clazz.find_by_id( params[:id])
+      end
     end
   end
 end
