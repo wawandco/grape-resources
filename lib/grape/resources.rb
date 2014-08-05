79b43972cbdd5fa212903b69188aa64e52390546
Antonio Pagano <apagano@wawand.co>
2014-08-05 10:14:47 -0500

    [cleaning] adding a funnier message on the AR message

--- lib/grape/resources.rb
+++ lib/grape/resources.rb
@@ -9,7 +9,7 @@ module Grape
         singular_name = clazz.name.underscore
         plural_name   = clazz.name.pluralize.underscore
 
-        raise Error("To use grape_resources on a given class it should inherit from ActiveRecord::Base.") unless clazz < ActiveRecord::Base
+        raise Error("To use grape_resources on a given class it should inherit from ActiveRecord::Base.( at least for now buddy ;) )") unless clazz < ActiveRecord::Base
 
         list_endpoint_for clazz
         get_endpoint_for clazz
