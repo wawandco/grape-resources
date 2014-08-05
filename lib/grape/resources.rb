379f6fc462bde47dfbfe3489710168cd9f7b052b
Antonio Pagano <apagano@wawand.co>
2014-08-05 10:13:56 -0500

    [cleaning] moving endpoint adding into separate methods

--- lib/grape/resources.rb
+++ lib/grape/resources.rb
@@ -11,25 +11,39 @@ module Grape
 
         raise Error("To use grape_resources on a given class it should inherit from ActiveRecord::Base.") unless clazz < ActiveRecord::Base
 
+        list_endpoint_for clazz
+        get_endpoint_for clazz
+        
+
+        route('POST', ["/#{singular_name}"], {})
+        route('PUT', ["/#{singular_name}/:id"], {})
+        route('DELETE', ["/#{singular_name}/:id"], {})
+      end
+
+      def list_endpoint_for(clazz)
+        plural_name   = clazz.name.pluralize.underscore
+
         route('GET', ["/#{plural_name}"], {} ) do                   
           result = Grape::Resources.list(clazz, params)
           result
         end
+      end
+
+      def get_endpoint_for(clazz)
+        singular_name = clazz.name.underscore
 
         route('GET', ["/#{singular_name}/:id"], {}) do
           result = Grape::Resources.find(clazz, params)
           error!( "#{singular_name} not found", 404) if result.nil?
           result
         end
-
-        route('POST', ["/#{singular_name}"], {})
-        route('PUT', ["/#{singular_name}/:id"], {})
-        route('DELETE', ["/#{singular_name}/:id"], {})
       end
+
     end
   end
 
   module Resources
+
     def self.list(clazz, params)
       result = clazz.all.to_a
     end
